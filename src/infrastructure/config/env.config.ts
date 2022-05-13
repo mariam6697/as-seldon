8040bd55e411270e746e1470a098800534a909d2
mariam6697 <mariam.maldonado.marin@gmail.com>
2022-05-13 00:42:53 -0400

    Initializing GitHub utils file

--- src/infrastructure/config/env.config.ts
+++ src/infrastructure/config/env.config.ts
@@ -5,5 +5,8 @@ dotenv.config();
 export const ENV = {
   port: process.env.PORT || '6060',
   mongoUrl: process.env.MONGO_URL,
-  jwtSecret: process.env.JWT_SECRET
+  jwtSecret: process.env.JWT_SECRET,
+  gitHubApiUrl: process.env.GITHUB_API_URL,
+  gitHubOrgName: process.env.GITHUB_ORG_NAME,
+  gitHubAccessToken: process.env.GITHUB_ACCESS_TOKEN
 };
---
+++ src/infrastructure/utils/github.utils.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { ENV } from '../config/env.config';
+
+interface Repository {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  html_url: string;
+  git_url: string;
+  ssh_url: string;
+  description: string;
+  url: string;
+}
+
+export default class GitHubUtils {
+  public static async createRepo(data: {
+    name: string;
+    description: string;
+    autoInit: boolean;
+    private: boolean;
+  }): Promise<Repository> {
+    const response = await axios.post(`${ENV.gitHubApiUrl}/orgs/${ENV.gitHubOrgName}/repos`, data);
+    console.log('response', response);
+    return {} as Repository;
+  }
+}
